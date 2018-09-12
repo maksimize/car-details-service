c63b208c8b7daa8998ee5379e2ddee5907c046fa
Maks <mahmoud.adel@gmail.com>
2018-09-12 22:22:27 +0800

    bad branch

--- src/test/java/com/maks/domain/DomainTests.java
+++ src/test/java/com/maks/domain/DomainTests.java
@@ -12,7 +12,7 @@ public class DomainTests {
     @Test
     public void testOwner() {
         Owner o1 = new Owner();
-        o1.setName("Kalles Grustransporter AB");
+        o1.setName("Kalles Grustransporter ABxxxxxx");
         o1.setAddress("Cementvägen 8, 111 11 Södertälje");
 
         assertEquals(o1.getName(), "Kalles Grustransporter AB");
